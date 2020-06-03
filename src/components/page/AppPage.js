950165c3dfa7f599c8eb66b4aaf27725d2b459c8
csh <csh@comcom.ai>
2020-06-03 11:24:30 +0900

    Update UI(Uploader)

--- src/components/page/AppPage.js
+++ src/components/page/AppPage.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import styled from 'styled-components';
-import Spinner from '../ui/Spinner';
+import Uploader from '../ui/Uploader';
 
 const Wrapper = styled.div`
     display: flex;
     align-items: center;
     flex-direction: column;
+    height: 100%;
+    background: #666666;
+    
+    @media (min-width: 1000px) {
+        width: 360px;
+    }
+    
+    @media (max-width: 1000px) {
+        width: 100vw;
+    }
+`;
+
+const UploaderWrapper = styled.div`
+    display: flex;
+    width: 100%;
+    flex-direction: row;
+    background: #F2F2F2;
 `;
 
 const HelloText = styled.text`
@@ -18,20 +35,28 @@ const HelloText = styled.text`
     letter-spacing: 0.15px;
 `;
 
-
 class AppPage extends React.Component {
     constructor() {
         super();
 
         this.state = {
+            images: [null, null],
         };
     }
 
     render() {
         return (
             <Wrapper>
+                <UploaderWrapper>
+                    <Uploader background='#2D9CDB' emoji='😜‍'
+                              image={this.state.images[0]}/>
+                    <Uploader background='#6FCF97' emoji='😜‍'
+                              image={this.state.images[1]}/>
+                </UploaderWrapper>
+
+
                 <HelloText>
-                    {"Hello world!"}
+                    {'Hello worldsssss!'}
                 </HelloText>
             </Wrapper>
         );
